154d22a62acae49de235e87b27e3baa26a1a34bd
ChangJin Wei (魏昌进) <WeiChangJin1996@gmail.com>
2025-12-26 23:45:41 +0800

    LICENSE

--- src/test/java/plus/wcj/jetbrains/plugins/java2flowchart/Condition.java
+++ src/test/java/plus/wcj/jetbrains/plugins/java2flowchart/Condition.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2025-present The original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package plus.wcj.jetbrains.plugins.java2flowchart;
 
 import java.util.List;
@@ -449,16 +465,7 @@ public class Condition {
     record Unknown(String reason) implements Shape {
     }
 
-    String switchPatternMatching(Shape s) {
-        // Java 21：switch pattern matching（含 case null）
-        return switch (s) {
-            case null -> "NULL";
-            case Circle c -> "Circle r=" + c.r();
-            case Rect r when r.w() > 0 && r.h() > 0 -> "Rect area=" + (r.w() * r.h()); // guard
-            case Rect r -> "Rect invalid";
-            case Unknown u -> "Unknown: " + u.reason();
-        };
-    }
+
 
     enum Gender {
         male, female
